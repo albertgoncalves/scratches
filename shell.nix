996e44bf68f242c32d6ee4a69919a568ebcac82a
albertgoncalves <goncalves.albert@gmail.com>
2019-03-25 08:51:55 -0400

    test on linux

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; mkShell {
     name = "Python";
-    buildInputs = [ python36
-                    python36Packages.matplotlib
-                    python36Packages.numpy
-                    python36Packages.flake8
-                    wget
-                  ];
+    buildInputs = [
+        (python37.withPackages(ps: with ps; [
+            matplotlib
+            numpy
+            flake8
+        ]))
+        wget
+    ];
     shellHook = ''
         alias flake8="flake8 --ignore E124,E128,E201,E203,E241,E731,W503"
-
         dir="data"
         if [ ! -e ./$dir/ ]; then
             mkdir $dir
