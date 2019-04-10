41cdf87ea545acf74418080fec4d049a18aaae17
albertgoncalves <goncalves.albert@gmail.com>
2019-04-10 01:01:37 -0400

    it works

--- src/GaussianPDF.hs
+++ src/GaussianPDF.hs
@@ -2,46 +2,54 @@
 
 import Text.Printf (printf)
 
-printMaybe :: Maybe String -> IO ()
-printMaybe = maybe (return ()) putStrLn
-
 showFloat :: Float -> String
 showFloat = printf "%.8f"
 
-mean :: (Integral a, Floating b) => a -> [b] -> b
-mean n xs = sum xs / fromIntegral n
+mean :: (Integral a, Floating b) => a -> [b] -> Maybe b
+mean _ [] = Nothing
+mean 0 _ = Nothing
+mean n xs = Just (sum xs / fromIntegral n)
 
 std :: Floating a => Int -> [a] -> Maybe a
 std _ [] = Nothing
 std _ [_] = Nothing
-std d xs = (Just . sqrt . mean (n - d) . map f) xs
+std d xs =
+    mean n xs
+    >>= \mean' -> mean (n - d) (map (f mean') xs)
+    >>= Just . sqrt
   where
     n = length xs
-    f = (** 2) . (mean n xs -)
+    f x = (** 2) . (x -)
 
-gaussianPDF :: Floating a => a -> a -> a -> a
-gaussianPDF mu sigma x = (1 / denom) * expon
+gaussianPDF :: (Eq a, Floating a) => a -> a -> a -> Maybe a
+gaussianPDF _ 0 _ = Nothing
+gaussianPDF mu sigma x = Just $ (1 / denom) * expon
   where
     sigma' = sigma ** 2
     expon = (exp . negate) $ ((x - mu) ** 2) / (sigma' * 2)
-    denom = sqrt $ 2 * pi * sigma'
+    denom = sqrt (2 * pi * sigma')
 
 {-
  -  $ R
  -  > xs = ...
  -  > dnorm(xs, mean(xs), sd(xs))
  -}
-mapGP :: Floating a => [a] -> Maybe [a]
-mapGP xs = f <$> std 1 xs
-  where
-    n = length xs
-    mu = mean n xs
-    f sigma = map (gaussianPDF mu $ sigma) xs
+
+autoGPDF:: (Eq a, Floating a) => [a] -> Maybe [a]
+autoGPDF xs =
+    mean (length xs) xs
+    >>= \mu -> std 1 xs
+    >>= \sigma -> mapM (gaussianPDF mu sigma) xs
+
+pipeline :: [Float] -> IO ()
+pipeline =
+    putStrLn
+    . maybe "Nothing" (unlines . map showFloat)
+    . autoGPDF
 
 main :: IO ()
-main = f xs
+main = pipeline xs
   where
-    f = printMaybe . (unlines <$>) . (map showFloat <$>) . mapGP
     xs =
         [ -0.9924723
         , -0.1592349
